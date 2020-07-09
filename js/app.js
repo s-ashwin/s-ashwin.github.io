c31ed1711f35dff6d6ddb9717762f1248c8d8cd8
s-ashwin <ashwin.sivanagarajan@gmail.com>
2020-07-09 13:42:50 +0530

    Fixed some issues with animation

--- js/app.js
+++ js/app.js
@@ -1,4 +1,10 @@
 window.addEventListener('load', ()=>{
     const load = document.querySelector('.load');
     load.classList.add('load-finish'); 
+    const homepage = document.querySelectorAll('.anim')
+    if(homepage){
+        for(let i=0; i<homepage.length; i++){
+            homepage[i].classList.add('animate')
+        }
+    }
 });
